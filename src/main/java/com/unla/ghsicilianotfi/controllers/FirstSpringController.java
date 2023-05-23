6313ba29c25327595f8aa3f7fba2e778e92b9d5e
Nicolas Naceli <naceli.cuenta.profesional@gmail.com>
2023-05-22 22:46:02 -0300

    added firstSpringController

---
+++ src/main/java/com/unla/ghsicilianotfi/controllers/FirstSpringController.java
@@ -0,0 +1,15 @@
+package com.unla.ghsicilianotfi.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/fistspring")
+public class FirstSpringController {
+	
+	@GetMapping("helloworld")
+	public String helloWorld() {
+		return "helloworld";
+	}
+}
