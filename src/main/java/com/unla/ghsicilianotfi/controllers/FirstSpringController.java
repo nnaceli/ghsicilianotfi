4b77e46cd9c1f4b7a6379db3043cde269f261866
Nicolas Naceli <naceli.cuenta.profesional@gmail.com>
2023-05-28 00:18:47 -0300

    boostrap template - Modern busssines added

--- src/main/java/com/unla/ghsicilianotfi/controllers/FirstSpringController.java
+++ src/main/java/com/unla/ghsicilianotfi/controllers/FirstSpringController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/fistspring")
+@RequestMapping("/firstspring")
 public class FirstSpringController {
 	
 	@GetMapping("helloworld")
